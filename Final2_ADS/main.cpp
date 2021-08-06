a7d0cf7dda398c1e222905f07504fc32ad309578
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2021-08-06 01:48:21 -0700

    Initial Commit

---
+++ Final2_ADS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Final2_ADS
+//
+//  Created by Nicholas Buras on 8/6/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
